5d857d67595bd0988a843f7f0420ecb750986797
balsam <balsamlhi50@gmail.com>
2025-11-11 00:06:28 +0100

    interface IGestion

--- src/Employe.java
+++ src/Employe.java
@@ -1,2 +1,74 @@
 public class Employe {
+    int id ;
+    String nom;
+    String prenom;
+    String nomDepartement;
+    int grade;
+
+    public Employe() {
+        this.id = 0;
+        this.nom = "";
+        this.prenom = "";
+        this.nomDepartement = "";
+        this.grade = 0;
+    }
+
+    public Employe(int id, String nom, String prenom, String nomDepartement, int grade) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.nomDepartement = nomDepartement;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNomDepartement() {
+        return nomDepartement;
+    }
+
+    public void setNomDepartement(String nomDepartement) {
+        this.nomDepartement = nomDepartement;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", nomDepartement='" + nomDepartement + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
