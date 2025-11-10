253a0e5728dbf3d37a075d671d4ad1e5f017fd2a
balsam <balsamlhi50@gmail.com>
2025-11-11 00:28:11 +0100

    collection

---
+++ src/SocieteArrayList.java
@@ -0,0 +1,62 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SocieteArrayList implements IGestion<Employe> {
+    ArrayList<Employe> listeEmployes;
+
+    public SocieteArrayList() {
+        listeEmployes = new ArrayList<>();
+    }
+
+    @Override
+    public void ajouterEmploye(Employe e) {
+        listeEmployes.add(e);
+        System.out.println("employee ajouté:"+ e );
+    }
+
+    @Override
+    public boolean rechercherEmploye(String nom) {
+        for (Employe e : listeEmployes) {
+            if (e.getNom().equals(nom)) {
+                System.out.println("Employé trouvé : " + e);
+                return true;
+            }
+        }
+        System.out.println("Aucun employé avec le nom : " + nom);
+        return false;
+    }
+
+    @Override
+    public boolean rechercherEmploye(Employe employe) {
+        return listeEmployes.contains(employe);
+    }
+
+    @Override
+    public void supprimerEmploye(Employe employe) {
+        listeEmployes.remove(employe);
+
+    }
+    public void displayEmploye(Employe employe) {
+        for (Employe e : listeEmployes) {
+            System.out.println(e);
+        }
+
+    }
+
+    @Override
+    public void trierEmployeParId() {
+        Collections.sort(listeEmployes, Comparator.comparingInt(Employe::getId));
+        System.out.println("Liste triée par ID.");
+    }
+
+    @Override
+    public void trierEmployeParNomDépartementEtGrade() {
+        Collections.sort(listeEmployes, Comparator
+                .comparing(Employe::getNomDepartement)
+                .thenComparingInt(Employe::getGrade)
+                .thenComparing(Employe::getNom));
+        System.out.println("Liste triée par departement,Grade,puis Nom");
+    }
+}
